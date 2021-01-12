0f6b0ea8ae79c121480f78f8ce757598d2f1b2c2
danielfrs87 <daniels@re.is>
2021-01-12 23:17:51 +0000

    🐛 Rewrote function

--- src/Api/api.js
+++ src/Api/api.js
@@ -1,17 +1,17 @@
 /* eslint-disable no-undef */
 import axios from 'axios';
 
-export async function customSearch(search, apiKey) {
-  // try {
-  //   const result = await axios({
-  //     method: 'get',
-  //     url: `https://content.googleapis.com/customsearch/v1?cx=001361074102112665899%3Ap7mybnrloug&q=${search}&searchType=image&key=${apiKey}`,
-  //   });
+export const customSearch = (search, apiKey) => {
+  try {
+    const result = await axios({
+      method: 'get',
+      url: `https://content.googleapis.com/customsearch/v1?cx=001361074102112665899%3Ap7mybnrloug&q=${search}&searchType=image&key=${apiKey}`,
+    });
 
-  //   return result;
-  // } catch (error) {
-  //   return error;
-  // }
+    return result;
+  } catch (error) {
+    return error;
+  }
 }
 
 export function test() {
