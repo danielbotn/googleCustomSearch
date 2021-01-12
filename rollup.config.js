5fdab9a776cdc3cd3f7b747c4f1f0558997c21e2
danielfrs87 <daniels@re.is>
2021-01-12 12:41:03 +0000

    🚧 Added env variables support for svelte

--- rollup.config.js
+++ rollup.config.js
@@ -6,6 +6,8 @@ import { terser } from 'rollup-plugin-terser';
 import sveltePreprocess from 'svelte-preprocess';
 import typescript from '@rollup/plugin-typescript';
 import css from 'rollup-plugin-css-only';
+import replace from '@rollup/plugin-replace';
+import includeEnv from "svelte-environment-variables";
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -43,6 +45,9 @@ export default {
     file: 'public/build/bundle.js',
   },
   plugins: [
+    replace({
+      ...includeEnv(),
+    }),
     svelte({
       preprocess: sveltePreprocess({ postcss: true }),
       compilerOptions: {
