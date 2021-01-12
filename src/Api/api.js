9bed8ed2361f92e41fbdebae6c9e1684f44d5974
danielfrs87 <daniels@re.is>
2021-01-12 23:15:03 +0000

    🐛 Could env be the bug?

--- src/Api/api.js
+++ src/Api/api.js
@@ -2,16 +2,16 @@
 import axios from 'axios';
 
 export async function customSearch(search, apiKey) {
-  try {
-    const result = await axios({
-      method: 'get',
-      url: `https://content.googleapis.com/customsearch/v1?cx=001361074102112665899%3Ap7mybnrloug&q=${search}&searchType=image&key=${apiKey}`,
-    });
+  // try {
+  //   const result = await axios({
+  //     method: 'get',
+  //     url: `https://content.googleapis.com/customsearch/v1?cx=001361074102112665899%3Ap7mybnrloug&q=${search}&searchType=image&key=${apiKey}`,
+  //   });
 
-    return result;
-  } catch (error) {
-    return error;
-  }
+  //   return result;
+  // } catch (error) {
+  //   return error;
+  // }
 }
 
 export function test() {
